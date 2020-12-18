cff1c415529c5ace337ba979e91a878d1005657f
yw6870 <68420090+yw6870@users.noreply.github.com>
2020-12-18 14:42:39 +0900

    バリデーション変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,9 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   
   validates :nickname, presence: true
-  validates :first_name_hiragana, presence: true
-  validates :last_name_hiragana, presence: true
-  validates :first_name_katakana, presence: true
-  validates :last_name_katakana, presence: true
+  validates :last_name, presence: true
+  validates :first_name, presence: true
+  validates :last_name_kana, presence: true
+  validates :first_name_kana, presence: true
   validates :birthday, presence: true
 end
