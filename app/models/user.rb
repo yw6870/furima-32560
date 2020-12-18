be97923b083bf0bbdaf7885a5e405f84410ba314
yw6870 <68420090+yw6870@users.noreply.github.com>
2020-12-18 14:33:59 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,9 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   
   validates :nickname, presence: true
-  validates :profile, presence: true
-  validates :occupation, presence: true
-  validates :position, presence: true
-
-
+  validates :first_name_hiragana, presence: true
+  validates :last_name_hiragana, presence: true
+  validates :first_name_katakana, presence: true
+  validates :last_name_katakana, presence: true
+  validates :birthday, presence: true
 end
--- db/migrate/20201218045415_devise_create_users.rb
+++ db/migrate/20201218045415_devise_create_users.rb
@@ -6,6 +6,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+      
       t.string :nickname, null: false
       t.string :first_name_hiragana, null: false
       t.string :last_name_hiragana, null: false
