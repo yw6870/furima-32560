6fd0a2b1242f738e39d5ff388250ad8df5bf1980
yw6870 <68420090+yw6870@users.noreply.github.com>
2020-12-23 21:14:28 +0900

    修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,11 +5,11 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :email, presence: true, uniqueness: true, inclusion: { in: "@" }
-  validates :encrypted_password, presence: true, length: { minimum: 6 }
   validates :last_name, presence: true
   validates :first_name, presence: true
   validates :last_name_kana, presence: true
   validates :first_name_kana, presence: true
   validates :birth_date, presence: true
+
+
 end
