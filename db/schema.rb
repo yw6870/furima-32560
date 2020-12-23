8c3132fb3ce45b8813492dc04e282210786f2d1a
yw6870 <68420090+yw6870@users.noreply.github.com>
2020-12-23 18:18:05 +0900

    マイグレーションファイル作成

--- db/schema.rb
+++ db/schema.rb
@@ -20,7 +20,7 @@ ActiveRecord::Schema.define(version: 2020_12_18_063051) do
     t.string "first_name", null: false
     t.string "last_name_kana", null: false
     t.string "first_name_kana", null: false
-    t.string "birthday", null: false
+    t.date "birth_date", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
