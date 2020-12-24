c06ac37649a86660fb6d40a378df6dc9b3252d87
yw6870 <68420090+yw6870@users.noreply.github.com>
2020-12-24 21:16:42 +0900

    Update 20201224064242_devise_create_users.rb

--- db/migrate/20201224064242_devise_create_users.rb
+++ db/migrate/20201224064242_devise_create_users.rb
@@ -7,7 +7,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       t.string :email,              null: false, unique: true, default: ""
       t.string :encrypted_password, null: false, default: ""
 
-      t.string :nickname,        null: false, unique: true
+      t.string :nickname,        null: false
       t.string :last_name,       null: false
       t.string :first_name,      null: false
       t.string :last_name_kana,  null: false
