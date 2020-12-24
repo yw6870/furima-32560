1093284a7cb83e77ef745c08e53894024d66d71b
yw6870 <68420090+yw6870@users.noreply.github.com>
2020-12-24 16:15:07 +0900

    ログイン機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,13 +3,5 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
-  validates :nickname, presence: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :last_name_kana, presence: true
-  validates :first_name_kana, presence: true
-  validates :birth_date, presence: true
-
-
+         
 end
