3e0f83ef59b476998ca964e6546da998793f55ce
yw6870 <68420090+yw6870@users.noreply.github.com>
2020-12-24 16:41:10 +0900

    バリデーションの設定

--- db/migrate/20201224064242_devise_create_users.rb
+++ db/migrate/20201224064242_devise_create_users.rb
@@ -4,10 +4,10 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :email,              null: false, default: ""
+      t.string :email,              null: false, unique: true, default: ""
       t.string :encrypted_password, null: false, default: ""
 
-      t.string :nickname,        null: false
+      t.string :nickname,        null: false, unique: true
       t.string :last_name,       null: false
       t.string :first_name,      null: false
       t.string :last_name_kana,  null: false
