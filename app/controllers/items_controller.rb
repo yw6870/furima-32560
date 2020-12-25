40269b9c58be0ab03124c0669a4ae9b7b305a392
yw6870 <68420090+yw6870@users.noreply.github.com>
2020-12-25 14:10:29 +0900

    バリデーションの設定、コントローラーの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,4 +5,9 @@ class ItemsController < ApplicationController
   def new
     @item = Item.new
   end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :info, :category, :sales_status_id, :shipping_fee_status_id, :prefecture_id, :scheduled_delivery_id, :price, :image).merge(user_id: current_user.id)
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -2,8 +2,13 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
-  validates :title, presence: true
-  validates :catch_copy, presence: true
-  validates :concept, presence: true  
-  validates :image, presence: true
+  validates :name,                   presence: true
+  validates :info,                   presence: true
+  validates :category_id,            presence: true
+  validates :sales_status_id,        presence: true
+  validates :shipping_fee_status_id, presence: true
+  validates :prefecture_id,          presence: true
+  validates :scheduled_delivery_id,  presence: true
+  validates :price,                  presence: true
+  validates :image,                  presence: true
 end
