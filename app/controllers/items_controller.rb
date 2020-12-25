e171e475f22c53d837e47a3c39c454875cd834e5
yw6870 <68420090+yw6870@users.noreply.github.com>
2020-12-25 16:41:00 +0900

    Active Hashのマイグレーションファイルの作成、マイグレーションファイルの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -17,6 +17,6 @@ class ItemsController < ApplicationController
 
   private
   def item_params
-    params.require(:item).permit(:name, :info, :category, :sales_status_id, :shipping_fee_status_id, :prefecture_id, :scheduled_delivery_id, :price, :image).merge(user_id: current_user.id)
+    params.require(:item).permit(:name, :info, :category_id, :sales_status_id, :shipping_fee_status_id, :prefecture_id, :scheduled_delivery_id, :price, :image).merge(user_id: current_user.id)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -2,13 +2,28 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :sales_status
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :shipping_fee_status
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :prefecture
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :scheduled_delivery
+
   validates :image,                  presence: true
   validates :name,                   presence: true
   validates :info,                   presence: true
-  validates :category_id,            presence: true
-  validates :sales_status_id,        presence: true
-  validates :shipping_fee_status_id, presence: true
-  validates :prefecture_id,          presence: true
-  validates :scheduled_delivery_id,  presence: true
+  validates :category_id,            presence: true, numericality: { other_than: 1 } 
+  validates :sales_status_id,        presence: true, numericality: { other_than: 1 } 
+  validates :shipping_fee_status_id, presence: true, numericality: { other_than: 1 } 
+  validates :prefecture_id,          presence: true, numericality: { other_than: 1 } 
+  validates :scheduled_delivery_id,  presence: true, numericality: { other_than: 1 } 
   validates :price,                  presence: true
 end
