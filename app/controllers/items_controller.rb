7b4803094b433c4aa7d39243ec1fa212567cf58a
yw6870 <68420090+yw6870@users.noreply.github.com>
2020-12-29 17:46:13 +0900

    指摘事項の修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
+  before_action :set_item, only: [:show, :edit, :update]
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :contributor_confirmation, only: [:edit, :update, :destroy]
+  
 
   def index
     @items = Item.order('created_at DESC')
@@ -26,10 +27,12 @@ class ItemsController < ApplicationController
 
   def edit
     @item = Item.find(params[:id])
+    redirect_to root_path unless current_user == @item.user
   end
 
   def update
     @item = Item.find(params[:id])
+    redirect_to root_path unless current_user == @item.user
     if @item.update(item_params)
       redirect_to item_path(@item)
     else
@@ -43,7 +46,7 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:name, :info, :category_id, :sales_status_id, :shipping_fee_status_id, :prefecture_id, :scheduled_delivery_id, :price, :image).merge(user_id: current_user.id)
   end
 
-  def contributor_confirmation
-    redirect_to root_path unless current_user == item.user
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
