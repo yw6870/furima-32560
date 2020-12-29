92899e548286bffb5abf613d4e99a061384ce739
yw6870 <68420090+yw6870@users.noreply.github.com>
2020-12-29 14:27:05 +0900

    商品詳細ページの編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, except: :index
+  before_action :authenticate_user!, except: [:index, :show]
 
   def index
     @items = Item.order("created_at DESC")
@@ -20,6 +20,7 @@ class ItemsController < ApplicationController
 
   def show
     @item = Item.find(params[:id])
+    @purchase_history = PurchaseHistory.all
   end
 
   private
---
+++ spec/factories/purchase_histories.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :purchase_history do
+    
+  end
+end
